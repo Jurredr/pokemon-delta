3f091df66416e30d572c1154d6aed4dcd857d9ce
Jurredr <github@jurre.me>
2021-07-12 20:14:38 +0200

    Initialized all entity components needed for Player

--- public/world/entity/PlayerEntity.js
+++ public/world/entity/PlayerEntity.js
@@ -1,11 +1,33 @@
 import Entity from './Entity'
+import Position from '../components/Position'
+import EntityAnimator from './components/EntityAnimator'
+import EntityMovement from './components/EntityMovement'
+import UserMovementController from '../../controller/UserMovementController'
 
 export default class PlayerEntity extends Entity {
-    // map
+    // position
+    // animator
+    // movement
+    // movementController
 
-    constructor(id, map) {
+    constructor(id, tileset) {
         super(id, true)
 
-        this.map = map
+        this.position = new Position(4, 2)
+        this.position.imgOffsetY = -16
+
+        this.animator = new EntityAnimator(this.position, tileset, 0, 8)
+
+        this.movement = new EntityMovement(this.animator, this.position)
+        this.movementController = new UserMovementController(this.movement)
+    }
+
+    update(deltaTime) {
+        this.movement.update(deltaTime)
+        this.movementController.update()
+    }
+
+    draw() {
+        this.animator.draw()
     }
 }

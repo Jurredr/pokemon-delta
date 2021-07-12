96dfcbd88fc3fdae193b313828896c05bb1655ec
Jurredr <github@jurre.me>
2021-07-13 01:49:38 +0200

    Added util function to get facing tile from direction

--- public/world/components/Position.js
+++ public/world/components/Position.js
@@ -1,3 +1,5 @@
+import Direction from './Direction'
+
 export default class Position {
     constructor(x, y) {
         this.x = x
@@ -5,5 +7,27 @@ export default class Position {
 
         this.imgOffsetX = 0
         this.imgOffsetY = 0
+    }
+
+    facingTile(direction) {
+        let position = undefined
+        switch (direction) {
+            case Direction.NORTH:
+                position = new Position(this.x, this.y - 1)
+                break
+            case Direction.EAST:
+                position = new Position(this.x + 1, this.y)
+                break
+            case Direction.SOUTH:
+                position = new Position(this.x, this.y + 1)
+                break
+            case Direction.WEST:
+                position = new Position(this.x - 1, this.y)
+                break
+        }
+
+        position.imgOffsetX = this.imgOffsetX
+        position.imgOffsetY = this.imgOffsetY
+        return position
     }
 }

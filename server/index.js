a79073057842eb0ddba0c1099012248a2d2813b9
Jurredr <github@jurre.me>
2021-07-13 15:50:01 +0200

    Moved server logic to separate file

--- server/index.js
+++ server/index.js
@@ -1,27 +1,8 @@
-// Import express
-import express from 'express'
-
-// Import mongoose
-import mongoose from 'mongoose'
+import Server from './Server'
+import dotenv from 'dotenv'
 
 // Configure dotenv
-require('dotenv').config()
+dotenv.config()
 
-// Create express instance
-const app = express()
-
-// Parse application/x-www-form-urlencoded and application/json
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-
-// Start standalone server if directly running
-if (require.main === module) {
-    const port = process.env.PORT || 3001
-    app.use(express.static('dist/public'))
-    app.listen(port, () => {
-        console.log(`Delta server listening on port ${port}`)
-    })
-}
-
-// Export the express app
-module.exports = app
+// Initialize the server
+new Server(process.env.PORT || 3000)

be6b721d06637d426d078aa966581bb83f738e95
Jurredr <github@jurre.me>
2021-07-14 10:15:27 +0200

    Removed all references to public from common

--- common/packet/PlayerLoginPacket.js
+++ common/packet/PlayerLoginPacket.js
@@ -1,14 +1,10 @@
-import Position from '../../public/world/components/Position'
-import Map from '../../public/world/map/Map'
-
 // Sent by a client after logging in
 export default class PlayerLoginPacket {
     /**
      *
      * @param {String} id
      * @param {String} username
-     * @param {Map} currentMap
-     * @param {Position} position
+     * @param {String} currentMap
      */
     constructor(id, username, currentMap, position) {
         this.id = id
--- common/packet/PlayerSpawnPacket.js
+++ common/packet/PlayerSpawnPacket.js
@@ -1,11 +1,8 @@
-import Position from '../../public/world/components/Position'
-
 export default class PlayerSpawnPacket {
     /**
      *
      * @param {String} playerId
      * @param {String} mapName
-     * @param {Position} position
      * @param {number} animatorY
      */
     constructor(playerId, mapName, position, animatorY) {
